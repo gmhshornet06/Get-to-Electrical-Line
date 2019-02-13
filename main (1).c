e153652342173e394081dc73b41ce552d1712c64
gmhshornet06 <47538676+gmhshornet06@users.noreply.github.com>
2019-02-13 13:58:44 -0500

    Update main (1).c

--- main (1).c
+++ main (1).c
@@ -6,22 +6,22 @@ int main ()
 	//shut_down_in(15);
 	create_connect();
 	while(analog(1)<2600)
-		{
-			create_drive_direct(250,250);
-			msleep(10);
-		}
-set_create_total_angle(0);
-    while(abs(get_create_total_angle())<40)
-    {
-        create_drive_direct(-150,150);
-        msleep(10);
-    }      
-    while(get_create_rfcliff_amt()>1900)
-    {
-        create_drive_direct(150,150);
-        msleep(10);
-    }
-    create_stop();
-    create_disconnect();
-    return 0;
+	{
+		create_drive_direct(250,250);
+		msleep(10);
+	}
+	set_create_total_angle(0);
+    	while(abs(get_create_total_angle())<40)
+    	{
+        	create_drive_direct(-150,150);
+        	msleep(10);
+    	}      
+    	while(get_create_rfcliff_amt()>1900)
+    	{
+        	create_drive_direct(150,150);
+        	msleep(10);
+    	}
+    	create_stop();
+    	create_disconnect();
+    	return 0;
 }
