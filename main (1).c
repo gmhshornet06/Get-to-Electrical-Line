45709544f2075c534b9ed132607ccbb54dc926ed
gmhshornet06 <47538676+gmhshornet06@users.noreply.github.com>
2019-02-25 13:57:23 -0500

    Revised Motion Code

--- main (1).c
+++ main (1).c
@@ -5,6 +5,20 @@ int main ()
 	//wait_for_light(3);
 	//shut_down_in(15);
 	create_connect();
+	set_create_total_angle(0);
+    	while(abs(get_create_total_angle())<25)
+    	{
+        	create_drive_direct(-150,150);
+        	msleep(10);
+    	}      
+	create_drive_direct(150,150);
+	msleep(1000);
+	set_create_total_angle(0);
+	while(abs(get_create_total_angle())<25)
+    	{
+        	create_drive_direct(150,-150);
+        	msleep(10);
+    	}      
 	while(analog(1)<2600)
 	{
 		create_drive_direct(250,250);
@@ -22,9 +36,9 @@ int main ()
         	msleep(10);
     	}
     	create_stop();
-	mav(0,500);
-	msleep(3000);
-	ao();
+	//mav(0,500);
+	//msleep(3000);
+	//ao();
     	create_disconnect();
     	return 0;
 }
